957a4a2b35ec50752de3794575d9a26b57193d3d
Galleons <88185941+Galleons2029@users.noreply.github.com>
2023-03-27 16:53:21 +0800

    Prototype
    Prototype of the web

---
+++ djangoProject/learning_logs/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+
+"""Register Topic with the admin site."""
+
+from .models import Topic
+
+admin.site.register(Topic)
+
+"""
+    This code first imports the model we want to register, Topic. 
+The dot in front of models tells Django to look for models.py 
+in the same directory as admin.py. 
+    The code admin.site.register() tells Django to manage our model 
+through the admin site.
+"""
