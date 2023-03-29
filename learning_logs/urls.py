005e2c0fe4b439dc268c62b6ba952508d3f90260
Galleons <88185941+Galleons2029@users.noreply.github.com>
2023-03-29 14:53:51 +0800

    new update

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page that shows all topics.
     path('topics/', views.topics, name='topics'),
-    """When Django examines a requested URL, this pattern 
-    will match any URL that has the base URL followed by topics."""
+    # When Django examines a requested URL, this pattern
+    # will match any URL that has the base URL followed by topics.
+
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
 ]
 
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -13,3 +13,16 @@ def topics(request):
     topics = Topic.objects.order_by('data_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries.
+    The function accepts the value captured by the expression /<int:topic_id>/
+    and assigns it to topic_id.
+    """
+    topic = Topic.objects.get(id=topic_id)   # retrieve the topic
+    entries = topic.entry_set.order_by('-date_added')
+    # sign"-" sorts the results in reverse order,
+    # display the most recent entries first.
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
