c0ad66d3c4edc7f4f7cf25144df5d7cc661d0919
Galleons <88185941+Galleons2029@users.noreply.github.com>
2023-04-28 12:58:43 +0800

    new update

--- learning_logs/templates/tests.py
+++ learning_logs/templates/tests.py
@@ -15,23 +15,23 @@ driver = webdriver.Chrome()
 class WebpageTests(unittest.TestCase):
 
     def test_title(self):
-        driver.get(file_uri("counter.html"))
+        driver.get(file_uri("demo/counter.html"))
         self.assertEqual(driver.title, "Counter")
 
     def test_increase(self):
-        driver.get(file_uri("counter.html"))
+        driver.get(file_uri("demo/counter.html"))
         increase = driver.find_element(By.ID, "increase")
         increase.click()
         self.assertEqual(driver.find_element(By.TAG_NAME, "h1").text, "1")
 
     def test_decrease(self):
-        driver.get(file_uri("counter.html"))
+        driver.get(file_uri("demo/counter.html"))
         decrease = driver.find_element(By.ID, "decrease")
         decrease.click()
         self.assertEqual(driver.find_element(By.TAG_NAME, "h1").text, "-1")
 
     def test_multiple_increase(self):
-        driver.get(file_uri("counter.html"))
+        driver.get(file_uri("demo/counter.html"))
         increase = driver.find_element(By.ID, "increase")
         for i in range(3):
             increase.click()
