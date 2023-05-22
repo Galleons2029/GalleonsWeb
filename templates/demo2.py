a65d81bda06193ff3bee59e55a84d2e38dae92ed
Galleons <88185941+Galleons2029@users.noreply.github.com>
2023-05-22 15:45:27 +0800

    new update

---
+++ templates/demo2.py
@@ -0,0 +1,20 @@
+import gradio as gr
+import random
+import time
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.Button("Clear")
+
+    def respond(message, chat_history):
+        bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+        chat_history.append((message, bot_message))
+        time.sleep(1)
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+    clear.click(lambda: None, None, chatbot, queue=False)
+
+demo.launch()
+
